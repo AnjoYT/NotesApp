2c2c8ebf045a8de8679809669976a2e2a3000889
AnjoYT <AnjoYT@wp.pl>
2024-01-23 22:39:48 +0100

    Adding functionality

--- Model/Note.cs
+++ Model/Note.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace NotesApp.Model
 {
-    internal class Note
+    internal class Note : ISerializable
     {
        private string? _title;
         private string? _description;
 
-        public string Title
+        public string? Title
         {
             get { return _title; }
             set { _title = value; }
         }
-        public string Description
+        public string? Description
         {
             get { return _description; }
             set { _description = value; }
+        }
+        public Note()
+        {
+
+        }
+        public Note(string? title,string? description)
+        {
+            _title = title;
+            _description = description;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("title", _title);
+            info.AddValue("description", _description);
         }
     }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,8 +1,11 @@
-﻿using NotesApp.Commands;
+﻿using Microsoft.Win32;
+using NotesApp.Commands;
+using NotesApp.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,44 +13,64 @@ using System.Windows.Input;
 
 namespace NotesApp.ViewModel
 {
-    internal class MainViewModel
-    { 
-        public ICommand SaveCommand { get; set; }
-        public ICommand OpenCommand { get; set; }
-        public ICommand LoadCommand { get; set; }
-        public ICommand NewCommand { get; set; }
-        public ICommand FindCommand { get; set; }
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string? _title;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string? Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string? Description { get; set; }
+        public ICommand SaveCommand { get;}
+        public ICommand OpenCommand { get;}
+        public ICommand NewCommand { get;}
+        public ICommand FindCommand { get;}
         public MainViewModel()
         {
             SaveCommand = new RelayCommands(Save);
             OpenCommand = new RelayCommands(Open);
-            LoadCommand = new RelayCommands(Load);
             NewCommand = new RelayCommands(New);
             FindCommand = new RelayCommands(Find);
         }
 
-        public void Save() 
+        public void Save()
         {
-            MessageBox.Show("Save");
+            Note note = new Note();
+            note.Title = Title;
+            note.Description = Description;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
         }
         public void Open()
         {
-            MessageBox.Show("Open");
-        }
-        public void Load()
-        {
-            MessageBox.Show("Load");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = "Document";
+            openFileDialog.DefaultExt = ".bc";
+            openFileDialog.Filter = "Bc documents (.bc)|*.bc";
+            bool? result = openFileDialog.ShowDialog();
+            if (result == true)
+            { string fileName = openFileDialog.FileName; }
         }
         public void New()
         {
-            MessageBox.Show("New");
+
         }
         public void Find()
         {
             MessageBox.Show("Find");
         }
 
-
-
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
